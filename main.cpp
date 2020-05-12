5c1a8734ba2a8dcc9d8a2bdffd73d4ca56cb5fa5
non258 <c0117037bf@edu.teu.ac.jp>
2020-05-13 08:53:28 +0900

    cg first commit

--- main.cpp
+++ main.cpp
@@ -2,18 +2,14 @@
 #include <iomanip>
 #include <iostream>
 
-using namespace std;
-
-int i,j,k;
-
 inline void mvm(double A[],double x[],double b[], int size)
 {
-  for(i=0;i<size;i++)
+  for(int i = 0; i < size; i++)
   {
-    b[i]=0.;
-    for(j=0;j<size;j++)
+    b[i] = 0.;
+    for(int j = 0; j < size; j++)
     {
-      b[i]+=A[i*size+j]*x[j];
+      b[i] += A[i*size+j] * x[j];
     }
   }
 }
@@ -21,24 +17,27 @@ inline void mvm(double A[],double x[],double b[], int size)
 inline double ip(double a[], double b[], int size)
 {
   double value=0.;
-  for(i=0;i<size;i++) value+=a[i]*b[i];
+  for(int i = 0; i < size; i++)
+  {
+    value += a[i]*b[i];
+  }
   return value;
 }
 
 int main()
 {
-  int size=3;
-  double alpha,beta;
-  double r0,rk,rk1;
-  double eps=pow(2,-50);
+  int size = 3;
+  double alpha, beta;
+  double r0, rk, rk1;
+  double eps = pow(2,-50);
 
-  double *r=new double[size];
-  double *p=new double[size];
-  double *b=new double[size];
-  double *x=new double[size];
-  double *Ax=new double[size];
-  double *Ap=new double[size];
-  double *A=new double[size*size];
+  double *r = new double[size];
+  double *p = new double[size];
+  double *b = new double[size];
+  double *x = new double[size];
+  double *Ax = new double[size];
+  double *Ap = new double[size];
+  double *A = new double[size*size];
 
 
   A[0*size+0]=1.;A[0*size+1]=2.;A[0*size+2]=-3.;
@@ -46,36 +45,55 @@ int main()
   A[2*size+0]=-3.;A[2*size+1]=-4.;A[2*size+2]=8.;
 
 
-  b[0]=-4;b[1]=-3;b[2]=12;
+  b[0] = -4;b[1] = -3;b[2] = 12;
 
-  for(i=0;i<size;i++) x[i]=0.;
+  for(int i = 0;i < size; i++) x[i]=0.;
 
-  mvm(A,x,Ax,size);
-  for(i=0;i<size;i++) r[i]=b[i]-Ax[i];
-  for(i=0;i<size;i++) p[i]=r[i];
-
-  r0=sqrt(ip(r,r,size));
-
-  for(k=0;k<100000;k++)
+  mvm(A, x, Ax, size);
+  for(int i = 0; i < size; i++)
   {
-    mvm(A,p,Ap,size);
-    alpha=ip(r,r,size)/ip(p,Ap,size);
-    rk=ip(r,r,size);
+    r[i] = b[i] - Ax[i];
+  }
+  for(int i = 0; i < size; i++)
+  {
+    p[i] = r[i];
+  }
 
-    for(i=0;i<size;i++) x[i]=x[i]+alpha*p[i];
-    for(i=0;i<size;i++) r[i]=r[i]-alpha*Ap[i];
+  r0 = sqrt(ip(r,r,size));
 
-    rk1=sqrt(ip(r,r,size));
+  for(int k = 0; k < 100000; k++)
+  {
+    mvm(A, p, Ap, size);
+    alpha = ip(r, r, size) / ip(p, Ap, size);
+    rk = ip(r, r, size);
+
+    for(int i = 0; i<size; i++)
+    {
+      x[i] = x[i] + alpha*p[i];
+    }
+    for(int i = 0; i<size; i++)
+    {
+      r[i] = r[i]-alpha*Ap[i];
+    }
+
+    rk1 = sqrt(ip(r,r,size));
     std::cout << "step: " << k << "  rk: " << rk1 << std::endl;
-    if(rk1/r0<=eps) break;
+    if(rk1 / r0 <= eps)
+      break;
 
-    beta=ip(r,r,size)/rk;
-    for(i=0;i<size;i++) p[i]=r[i]+beta*p[i];
+    beta = ip(r,r,size) / rk;
+    for(int i = 0; i < size; i++)
+    {
+      p[i] = r[i] + beta*p[i];
+    }
   }
 
   std::cout << eps << std::endl;
 
-  for(i=0;i<size;i++) cout<<x[i]<<endl;
+  for(int i = 0; i < size; i++)
+  {
+    std::cout << x[i] << std::endl;
+  }
 
   delete [] r,p,b,x,Ax,Ap,A;
 
